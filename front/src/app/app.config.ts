93f04f8a6c8a7ca1ec9b134822db0ec8f5fd627e
GeekGuste <manouauguste7@gmail.com>
2025-08-17 03:33:38 +0200

    Display elements

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -3,13 +3,16 @@ import { provideRouter } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environment/environment';
+import { API_BASE_URL } from './api/nesso-client-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideHttpClient(withInterceptorsFromDi()), 
+    provideHttpClient(), 
     provideRouter(routes),
-    importProvidersFrom(MarkdownModule.forRoot())
+    importProvidersFrom(MarkdownModule.forRoot()),
+    { provide: API_BASE_URL, useValue: environment.apiBaseUrl }
   ]
 };
--- front/src/app/core/service/recipe-service.service.ts
+++ front/src/app/core/service/recipe-service.service.ts
@@ -1,27 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Recipe } from '../model/recipe';
-import { Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Client, GetRecipeInput } from 'src/app/api/nesso-client-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   private http = inject(HttpClient);
-  // adapte selon ton env (ex. environments)
-  private readonly baseUrl = 'http://localhost:8000';
+  private api = inject(Client);
 
   searchRecipes(ingredients: string[]): Observable<Recipe[]> {
-    return of([{
-      recipeName: 'Galettes de courgette rôties à la mozzarella',
-      ingredients: ["2-3 courgettes",
-        "farine de blé",
-        "1 boule de mozzarella",
-        "crème fraîche (pour servir)",
-        "sel",
-        "poivre"],
-      preparation: '1. Préchauffer le four à 200 °C (chaleur tournante si possible).<br/>2. Laver puis râper les courgettes. Presser fortement la pulpe râpée pour éliminer un maximum d\'eau.',
-      cookingTime: '30 minutes'
-    }]);
+    return this.api.post({ingredients: ingredients} as GetRecipeInput).pipe(
+      // Map the API response to the Recipe model
+      map(response => response.recipes?.map(item => ({
+        recipeName: item.recipeName,
+        ingredients: item.ingredients,
+        preparation: item.preparation,
+        cookingTime: item.cookingTime
+      } as Recipe)) || []),
+      catchError(error => {
+        console.error('Error fetching recipes:', error);
+        return of([]); // Return an empty array on error
+      })
+    );
   }
 }
